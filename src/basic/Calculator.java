b9932025ebb6973a1bde31cb3a82cb243ddfdad6
saisrinivas081100 <kovvada.saisrinivas@worldline.com>
2022-08-18 16:58:45 +0530

    added math and calculator class

---
+++ src/basic/Calculator.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Calculator {
+public static void main(String[] args) {
+	System.out.println("Addition");
+
+    Math.add(22, 67);
+
+    Math.add(65, 89, 43);
+
+    System.out.println("Multiplication");
+
+    Math.multiply(23, 8);
+}
+}
